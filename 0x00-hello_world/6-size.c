9677ad2670e201cc41da27fd51b9aa9d7ccd9bcc
HExecutive <HExecutive.com>
2023-09-14 02:30:51 -0700

    size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int k;
 long long int l;
 float m;
 
-printf("size of a char %lu byte(s)\n",(unsigned long)sizeof(i));
-printf("size of an int %lu byte(s)\n",(unsigned long)sizeof(j));
-printf("size of a long int %lu byte(s)\n",(unsigned long)sizeof(k));
-printf("size of a long long int %lu byte(s)\n",(unsigned long)sizeof(l));
-printf("size of a float %lu byte(s)\n",(unsigned long)sizeof(m));
+printf("size of a char %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("size of an int %lu byte(s)\n", (unsigned long)sizeof(j));
+printf("size of a long int %lu byte(s)\n", (unsigned long)sizeof(k));
+iprintf("size of a long long int %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("size of a float %lu byte(s)\n", (unsigned long)sizeof(m));
 return (0);
 }
